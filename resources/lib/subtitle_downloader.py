2413940454d78be701b8532e52376b0d48921f6d
Tomy Kho <mail@tomykho.dev>
2021-12-01 20:31:30 +0800

    Add moviehash to search query

--- resources/lib/subtitle_downloader.py
+++ resources/lib/subtitle_downloader.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import sys
 import uuid
+import struct
 
 import xbmcaddon
 import xbmcgui
@@ -69,6 +70,12 @@ class SubtitleDownloader:
         else:
             media_data = get_media_data()
         self.query = {**media_data, **file_data, **language_data}
+
+        file_path = self.query['file_original_path']
+        rar = self.query['rar']
+
+        hash = self.hashFile(file_path, rar)
+        self.query['moviehash'] = hash
 
         try:
             self.subtitles = self.open_subtitles.search_subtitles(self.query)
@@ -136,3 +143,79 @@ class SubtitleDownloader:
 
             xbmcplugin.addDirectoryItem(handle=self.handle, url=url, listitem=list_item, isFolder=False)
         xbmcplugin.endOfDirectory(self.handle)
+
+    def hashFile(self, file_path, rar):
+        if rar:
+            return self.OpensubtitlesHashRar(file_path)
+
+        log( __name__,"Hash Standard file")
+        longlongformat = 'q'  # long long
+        bytesize = struct.calcsize(longlongformat)
+        f = xbmcvfs.File(file_path)
+
+        filesize = f.size()
+        hash = filesize
+
+        if filesize < 65536 * 2:
+            return "SizeError"
+
+        buffer = f.read(65536)
+        f.seek(max(0,filesize-65536),0)
+        buffer += f.read(65536)
+        f.close()
+        for x in range((65536/bytesize)*2):
+            size = x*bytesize
+            (l_value,)= struct.unpack(longlongformat, buffer[size:size+bytesize])
+            hash += l_value
+            hash = hash & 0xFFFFFFFFFFFFFFFF
+
+        returnHash = "%016x" % hash
+        return returnHash
+
+    def OpensubtitlesHashRar(self, firsrarfile):
+        log( __name__,"Hash Rar file")
+        f = xbmcvfs.File(firsrarfile)
+        a=f.read(4)
+        if a!='Rar!':
+            raise Exception('ERROR: This is not rar file.')
+        seek=0
+        for i in range(4):
+            f.seek(max(0,seek),0)
+            a=f.read(100)
+            type,flag,size=struct.unpack( '<BHH', a[2:2+5])
+            if 0x74==type:
+                if 0x30!=struct.unpack( '<B', a[25:25+1])[0]:
+                    raise Exception('Bad compression method! Work only for "store".')
+                s_partiizebodystart=seek+size
+                s_partiizebody,s_unpacksize=struct.unpack( '<II', a[7:7+2*4])
+                if (flag & 0x0100):
+                    s_unpacksize=(struct.unpack( '<I', a[36:36+4])[0] <<32 )+s_unpacksize
+                    log( __name__ , 'Hash untested for files biger that 2gb. May work or may generate bad hash.')
+                lastrarfile=getlastsplit(firsrarfile,(s_unpacksize-1)/s_partiizebody)
+                hash=addfilehash(firsrarfile,s_unpacksize,s_partiizebodystart)
+                hash=addfilehash(lastrarfile,hash,(s_unpacksize%s_partiizebody)+s_partiizebodystart-65536)
+                f.close()
+                return (s_unpacksize,"%016x" % hash )
+            seek+=size
+        raise Exception('ERROR: Not Body part in rar file.')
+
+    def getlastsplit(firsrarfile,x):
+        if firsrarfile[-3:]=='001':
+            return firsrarfile[:-3]+('%03d' %(x+1))
+        if firsrarfile[-11:-6]=='.part':
+            return firsrarfile[0:-6]+('%02d' % (x+1))+firsrarfile[-4:]
+        if firsrarfile[-10:-5]=='.part':
+            return firsrarfile[0:-5]+('%1d' % (x+1))+firsrarfile[-4:]
+        return firsrarfile[0:-2]+('%02d' %(x-1) )
+
+    def addfilehash(name,hash,seek):
+        f = xbmcvfs.File(name)
+        f.seek(max(0,seek),0)
+        for i in range(8192):
+            hash+=struct.unpack('<q', f.read(8))[0]
+            hash =hash & 0xffffffffffffffff
+        f.close()
+        return hash
+
+    def normalizeString(str):
+        return unicodedata.normalize('NFKD', str)
